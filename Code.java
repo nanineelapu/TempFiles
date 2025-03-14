a2a52aa0dc452a24091353a61df4610b2382711e
Nani Neelapu <neelapunani2580@gmail.com>
2025-03-14 17:43:17 +0530

    Okay

--- Code.java
+++ Code.java
@@ -1,48 +1,8 @@
-import java.util.Scanner;
-
 public class Code {
 
     public static void main(String[] args) {
-        //Casting
+        String title = "Java code";
 
-        // Premetive
-
-        // int 
-        // char
-        // boolean
-        // double
-
-        //references
-
-        //String
-        //Array
-        //Object
-
-        String name =  "Java";
-        String secName = "Code";
-
-        //Create a method
-
-        String okay = name + secName;
-
-        Scanner sc = new Scanner(System.in);
-
-        String title = sc.nextLine();
-
-
-        if (title == okay) {
-            System.out.println("Yes you can access the keys");
-        
-        } else {
-            System.out.println("Your input data is wrong");
-        }
-
-
-        // if () {
-            
-        // } else {
-            
-        // }
-
+        System.out.println(title);
     }
 }
